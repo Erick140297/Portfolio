12414455e370ddc75012ffd1666467a2e6141296
Erick140297 <erickmonterrubio67@outlook.com>
2023-03-16 20:18:24 -0600

    skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -4,18 +4,55 @@ import styled from "styled-components";
 
 const Skills = () => {
   const { state } = useContext(AppContext);
+  const darkTheme = state.darkTheme;
   const defaultLanguage = state.language;
+
+  const Logos = [
+    "https://res.cloudinary.com/dnrcmjyu1/image/upload/v1679013867/Logos/React_aunlxl.png",
+    "https://res.cloudinary.com/dnrcmjyu1/image/upload/v1679013867/Logos/JavaScript_vtxy84.png",
+
+    "https://res.cloudinary.com/dnrcmjyu1/image/upload/v1679013867/Logos/HTML_idmvlv.png",
+    "https://res.cloudinary.com/dnrcmjyu1/image/upload/v1679013867/Logos/CSS3_wf1gyo.png",
+    "https://res.cloudinary.com/dnrcmjyu1/image/upload/v1679013866/Logos/Bootstrap_t4jcrl.png",
+    "https://res.cloudinary.com/dnrcmjyu1/image/upload/v1679013866/Logos/Dart_b8pl6m.png",
+    "https://res.cloudinary.com/dnrcmjyu1/image/upload/v1679013866/Logos/Figma_c8zjdn.png",
+    "https://res.cloudinary.com/dnrcmjyu1/image/upload/v1679013866/Logos/Postgresql_edblny.png",
+    "https://res.cloudinary.com/dnrcmjyu1/image/upload/v1679013865/Logos/Redux_nvftrm.png",
+    "https://res.cloudinary.com/dnrcmjyu1/image/upload/v1679013865/Logos/Typescript_j3lzhd.png",
+    "https://res.cloudinary.com/dnrcmjyu1/image/upload/v1679013865/Logos/Nextjs_t5umbk.png",
+    "https://res.cloudinary.com/dnrcmjyu1/image/upload/v1679013867/Logos/MongoDB_no2okt.png",
+    "https://res.cloudinary.com/dnrcmjyu1/image/upload/v1679013867/Logos/Flutter_wr06ih.png",
+  ];
 
   return (
     <Container>
       {defaultLanguage === "EN" ? (
-        <h2 id="skills">Skills</h2>
+        <>
+          <h2 id="skills">Skills</h2>
+          <LogoContainer darktheme={darkTheme}>
+            {Logos.map((element, index) => {
+              return (
+                <Logo key={index} darktheme={darkTheme}>
+                  <img src={element} alt="logo" />
+                </Logo>
+              );
+            })}
+          </LogoContainer>
+        </>
       ) : (
-        <h2 id="about">Tecnologías</h2>
+        <>
+          <h2 id="about">Tecnologías</h2>
+          <LogoContainer darktheme={darkTheme}>
+            {Logos.map((element, index) => {
+              return (
+                <Logo key={index} darktheme={darkTheme}>
+                  <img src={element} alt="logo" />
+                </Logo>
+              );
+            })}
+          </LogoContainer>
+        </>
       )}
-      <div>
-        
-      </div>
     </Container>
   );
 };
@@ -35,15 +72,47 @@ const Container = styled.div`
   p {
     font-size: 20px;
   }
+
   @media screen and (max-width: 950px) {
     margin-bottom: 20px;
-
     h2 {
       font-size: 20px;
       margin-bottom: 10px;
     }
     p {
       font-size: 15px;
+    }
+  }
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  border-radius: 20px;
+  background: ${({ darktheme }) => (darktheme ? "#212020" : "#00b0ff")};
+`;
+
+const Logo = styled.div`
+  margin: 25px;
+  padding: 15px;
+  background: #ffffff;
+  border-radius: 5px;
+  border: 1px solid ${({ darktheme }) => (darktheme ? "#ff0000" : "#f50057")};
+  transition: all 0.3s ease;
+  img {
+    height: 70px;
+  }
+  &:hover {
+    background: ${({ darktheme }) => (darktheme ? "#ff0000" : "#f50057")};
+  }
+  @media screen and (max-width: 950px) {
+    margin: 5px;
+    img {
+      height: 30px;
     }
   }
 `;
